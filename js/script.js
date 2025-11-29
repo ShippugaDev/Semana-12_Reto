905dc9ca3d6e6d4aba2939031dbde64011d29863
Joel Gutierrez <joelgp18@gmail.com>
2025-11-29 13:52:00 -0500

    Reto_byJoelGP

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const mostrarMensajeBtn = document.getElementById('mostrarMensajeBtn');
+const addSkillBtn = document.getElementById('agregarHabilidadBtn');
+let contador = 1;
+mostrarMensajeBtn.addEventListener('click', () => {
+
+    if (contador == 1) {
+        document.getElementById("mensaje").style.display = "block";
+        document.getElementById("mensaje").innerText = "¡Felicidades, has hecho clic en el botón!";
+        contador++;
+    } else if (contador > 1) {
+        alert(`¡Ya hiciste clic en el botón! Intentos: ${contador - 1}`);
+        contador++;
+    }
+})
+
+addSkillBtn.addEventListener('click', () => {
+    const skillInput = prompt("Ingrese una nueva habilidad:");
+    let text = "";
+    if (skillInput) {
+        text += `<li  class="f-size-16 background-light-gray mg-0-0-10 pd-12 f-familiy-primary b-radius-8 t-align-center">${skillInput}</li>`;
+
+    } else {
+        alert("Por favor, ingresa una habilidad válida o diferente.");
+    }
+    document.getElementById("skills").innerHTML += text;
+
+
+})   
